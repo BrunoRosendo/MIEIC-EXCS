d220c5a3b1ec2c46d66e457c590c2763710def1c
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-03-24 18:44:18 +0000

    IART F3- auxiliary functions

--- IART/F3/connect-four.py
+++ IART/F3/connect-four.py
@@ -31,6 +31,27 @@ def playPiece(col):
     return True
 
 
+def getDiagonals():
+    diags = []
+    matrix = np.array(board)
+
+    for i in range(BOARD_NUM_COLS - 1):
+        npDiag = np.diag(matrix, i)
+        npDiag2 = np.diag(np.fliplr(matrix), i)
+
+        diags.append(npDiag2.tolist())
+        diags.append(npDiag.tolist())
+
+    for i in range(BOARD_NUM_ROWS):
+        npDiag = np.diag(matrix, -i - 1)
+        npDiag2 = np.diag(np.fliplr(matrix), -i -1)
+
+        diags.append(npDiag2.tolist())
+        diags.append(npDiag.tolist())
+
+    return diags
+
+
 def nlines4Vertical(player, matrix = board):
     numLines = 0
 
@@ -52,28 +73,54 @@ def nlines4Horizontal(player):
     return nlines4Vertical(player, transposedBoard)
 
 def nlines4Diagonal(player):
-    diags = []
-    matrix = np.array(board)
-
-    for i in range(BOARD_NUM_COLS - 1):
-        npDiag = np.diag(matrix, i)
-        npDiag2 = np.diag(np.fliplr(matrix), i)
-
-        diags.append(npDiag2.tolist())
-        diags.append(npDiag.tolist())
-
-    for i in range(BOARD_NUM_ROWS):
-        npDiag = np.diag(matrix, -i - 1)
-        npDiag2 = np.diag(np.fliplr(matrix), -i -1)
-
-        diags.append(npDiag2.tolist())
-        diags.append(npDiag.tolist())
-
+    diags = getDiagonals()
     return nlines4Vertical(player, diags)
 
 
 def nlines4(player):
     return nlines4Vertical(player) + nlines4Horizontal(player) + nlines4Diagonal(player)
+
+def isLine3(line, player):
+    return line.count(0) == 1 and line.count(player) == 3
+
+# This does not account for lines3 which could be part of a line4 (the cell after or before)
+def nlines3Vertical(player, matrix = board):
+    numLines = 0
+
+    for col in matrix:
+        i = 0
+        j = 4
+        while (j <= len(col)):
+            line = col[i:j]
+            if isLine3(line, player):
+                numLines += 1
+
+            i += 1
+            j += 1
+    return numLines
+
+
+def nlines3Horizontal(player):
+    transposedBoard = np.transpose(board).tolist()
+    return nlines3Vertical(player, transposedBoard)
+
+def nlines3Diagonal(player):
+    diags = getDiagonals()
+    return nlines3Vertical(player, diags)
+
+def nlines3(player):
+    return nlines3Vertical(player) + nlines3Horizontal(player) + nlines3Diagonal(player)
+
+
+def central(player):
+    points = 0
+    for i in range(-1, 2):
+        col = board[BOARD_NUM_COLS // 2 + i]
+        for cell in col:
+            if cell == player:
+                points += 2 if i == 0 else 1
+    return points
+
 
 def checkWinner():
     return nlines4(turn) > 0
